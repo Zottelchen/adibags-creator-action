4bd5e891c553f7cd9ade471e2f889dec611734cd
Zottelchen <github@zottelchen.com>
2023-07-01 17:35:37 +0200

    update gist pushing to hopefully be more stable
    Mostly because I don't have to do the encoding anymore.

--- github.py
+++ github.py
@@ -6,23 +6,29 @@ from datetime import datetime
 import requests
 
 
-def get_gist(gist_id=os.environ.get("GITHUB_GIST_ID", "b86d83d7b11377fb4a143d9cb12aef64")):
+def get_gist(
+    gist_id=os.environ.get("GITHUB_GIST_ID", "b86d83d7b11377fb4a143d9cb12aef64")
+):
     """Get a gist with a file named "items.json" from GitHub."""
     print("Getting Item Cache from Gist:", gist_id)
     headers = {
-        'Accept': 'application/vnd.github+json',
-        'X-GitHub-Api-Version': '2022-11-28',
+        "Accept": "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28",
     }
 
-    if os.environ.get("GITHUB_GIST_TOKEN"):  # this should make fetching private gists possible
-        headers['Authorization'] = f'Bearer {os.environ.get("GITHUB_GIST_TOKEN")}'
+    if os.environ.get(
+        "GITHUB_GIST_TOKEN"
+    ):  # this should make fetching private gists possible
+        headers["Authorization"] = f'Bearer {os.environ.get("GITHUB_GIST_TOKEN")}'
 
     attempt = 0
     max_attempts = 5
     response = None
     while attempt < max_attempts:
         try:
-            response = requests.get(f'https://api.github.com/gists/{gist_id}', headers=headers)
+            response = requests.get(
+                f"https://api.github.com/gists/{gist_id}", headers=headers
+            )
             # If the request was successful, break out of the loop
             response.raise_for_status()
             break
@@ -43,22 +49,30 @@ def get_gist(gist_id=os.environ.get("GITHUB_GIST_ID", "b86d83d7b11377fb4a143d9cb
         return {}
 
 
-def update_gist(item_cache: dict, gist_id=os.environ.get("GITHUB_GIST_ID", "b86d83d7b11377fb4a143d9cb12aef64")) -> str:
+def update_gist(
+    item_cache: dict,
+    gist_id=os.environ.get("GITHUB_GIST_ID", "b86d83d7b11377fb4a143d9cb12aef64"),
+) -> str:
     """Update the gist with a file named "items.json" on GitHub."""
     if not os.environ.get("GITHUB_GIST_TOKEN"):
         print("No GitHub Token found, skipping updating online item cache.")
         return "The item cache was updated. *Gist was not updated, because no GitHub Token was found.*"
 
     headers = {
-        'Accept': 'application/vnd.github+json',
-        'Authorization': f'Bearer {os.environ.get("GITHUB_GIST_TOKEN")}',
-        'X-GitHub-Api-Version': '2022-11-28',
-        'Content-Type': 'application/x-www-form-urlencoded',
+        "Accept": "application/vnd.github+json",
+        "Authorization": f'Bearer {os.environ.get("GITHUB_GIST_TOKEN")}',
+        "X-GitHub-Api-Version": "2022-11-28",
+        "Content-Type": "application/x-www-form-urlencoded",
     }
 
-    content = json.dumps(item_cache, sort_keys=True, indent=2).replace('"', '\\"').replace("\n", "\\n")
-    data = f'{{"description":"Last updated: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}","files":{{"items.json":{{"content":"{content}"}}}}}}'
-    response = requests.patch(f'https://api.github.com/gists/{gist_id}', headers=headers, data=data)
+    content = json.dumps(item_cache, sort_keys=True, indent=2)
+    data = {
+        "description": f"Last updated: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}",
+        "files": {"items.json": {"content": content}},
+    }
+    response = requests.patch(
+        f"https://api.github.com/gists/{gist_id}", headers=headers, json=data
+    )
     if response.status_code == 200:
         print("Successfully updated Item Cache Gist.")
         return "The item cache was updated. Gist was updated successfully."
